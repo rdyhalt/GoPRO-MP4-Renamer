ac2fa8d7d90b88994cd0591cfb14ae2574e5c5c6
Kim Togo Andersen <ka@miralix.com>
2019-02-04 14:47:59 +0100

    Setup project

---
+++ GoPRO-MP4-Renamer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoPRO_MP4_Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("GoPROMP4Renamer <source>");
+                return;
+            }
+
+            var sourceDir = new DirectoryInfo(args[0]);
+
+            if (sourceDir.Exists == false)
+            {
+                Console.WriteLine($"GoPROMP4Renamer <source> does not exists - {sourceDir.FullName}");
+                return;
+            }
+
+            foreach (FileInfo fileInfo in sourceDir.GetFiles("*.mp4"))
+            {
+                DateTime mediaCreated = fileInfo.CreationTime;
+                string newFileName = Path.Combine(sourceDir.FullName, $"Img{mediaCreated:yyyymmdd}_{mediaCreated:HHmmss}{fileInfo.Extension}");
+
+
+
+
+                
+            }
+        }
+    }
+}
